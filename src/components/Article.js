3fb24c10101f6ac7c620f1b53c7e74afa5122bb3
clement.gallois@epitech.eu <clement.gallois@epitech.eu>
2021-02-19 18:47:16 +0100

    Added more info to articles, added a basic grid

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,11 +1,57 @@
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
+const Thumbnail = styled.img`
+  width: 100%;
+`;
+
+const StyledArticle = styled.section`
+    &{
+      grid-column: span 3;
+
+      .description{
+        display: none;
+      }
+    }
+    &:first-child{
+      grid-row: 1/span 2;
+      grid-column: 1/span 6;
+      
+      .description{
+        display: block;
+      }
+    }
+    &:nth-child(n+6){
+      grid-column: span 2;
+    }
+`;
 const Article = ({
   // eslint-disable-next-line no-unused-vars
-  author, content, description, publishedAt, title, url, urlToImage,
-}) => (
-  <article className="article">{title}</article>
-);
+  author, content, description, publishedAt, title, url, urlToImage, source,
+}) => {
+  console.log(description && description.length);
+  console.log({
+    author, content, description, publishedAt, title, url, urlToImage, source,
+  });
+  return (
+    <StyledArticle className="article">
+      <Thumbnail src={urlToImage} alt={title} />
+      <h2>{title}</h2>
+      <div className="article-source">
+        {source?.name ? source.name : 'Unknown source'}
+        {' '}
+        {author && (
+          <span className="author">
+            by
+            {' '}
+            {author}
+          </span>
+        )}
+      </div>
+      <div className="description">{description || content}</div>
+    </StyledArticle>
+  );
+};
 
 Article.propTypes = {
   author: PropTypes.string,
@@ -15,6 +61,10 @@ Article.propTypes = {
   title: PropTypes.string,
   url: PropTypes.string,
   urlToImage: PropTypes.string,
+  source: PropTypes.shape({
+    id: PropTypes.string,
+    name: PropTypes.string,
+  }),
 };
 
 Article.defaultProps = {
@@ -25,6 +75,10 @@ Article.defaultProps = {
   title: '',
   url: '',
   urlToImage: '',
+  source: {
+    id: '',
+    name: '',
+  },
 };
 
 export default Article;
--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,15 +1,26 @@
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import Article from './Article';
+
+const Grid = styled.section`
+  display: grid;
+    -ms-flex-align: start;
+    -ms-flex-pack: justify;
+    grid-column-gap: 1.5rem;
+    grid-row-gap: 1.5rem;
+    grid-template-columns: repeat(12,1fr);
+    grid-auto-flow: dense;
+`;
 
 const ArticleList = ({ articlesData }) => {
   if (!articlesData || articlesData.length === 0) {
     return (<p>No Result to Display</p>);
   }
   return (
-    <section className="article-list">
+    <Grid>
       {/* eslint-disable-next-line react/jsx-props-no-spreading */ }
       {articlesData.map((article) => <Article key={article.url} {...article} />)}
-    </section>
+    </Grid>
   );
 };
 
