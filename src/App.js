48cb2fcfa68904b603f6470ab096298a8a46eb1c
clement.gallois@epitech.eu <clement.gallois@epitech.eu>
2021-02-20 13:28:24 +0100

    fill the search bar from query parameters

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import {
   Route,
   useHistory,
 } from 'react-router-dom';
+import { useState } from 'react';
 import styled from 'styled-components';
 import Home from './pages/Home';
 import Search from './pages/Search';
 import './App.css';
+import { useQueryParameters } from './hooks';
 
 const MainContent = styled.div`
   margin:auto;
@@ -15,9 +17,12 @@ const MainContent = styled.div`
 `;
 
 function SearchBar() {
+  const queryParameters = useQueryParameters();
+  const [value, setValue] = useState(queryParameters || '');
   const history = useHistory();
 
   const handleChangeSearchBar = (event) => {
+    setValue(event.target.value);
     const searchText = event.target.value.trim();
     if (searchText === '') {
       history.push('/');
@@ -25,7 +30,7 @@ function SearchBar() {
       history.push({ pathname: '/search', search: `?q=${searchText}` });
     }
   };
-  return <input className="search" type="text" placeholder="search" onChange={handleChangeSearchBar} />;
+  return <input className="search" type="text" placeholder="search" onChange={handleChangeSearchBar} value={value} />;
 }
 
 function App() {
