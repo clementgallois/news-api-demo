97538ba75060cbecc807680ec1baeb947c7e35b8
clement.gallois@epitech.eu <clement.gallois@epitech.eu>
2021-02-21 01:32:37 +0100

    reduce padding main content on lower screens

--- src/components/MainContent/index.js
+++ src/components/MainContent/index.js
@@ -4,6 +4,12 @@ const MainContent = styled.div`
   margin:auto;
   max-width:1140px;
   padding: 0 2.5rem;
+  @media (max-width: 719px){
+    padding: 2.5rem 1.5rem;
+}
+  @media (max-width: 599px){
+    padding: 1.5rem 1rem;
+}
 `;
 
 export default MainContent;
